defa8574111933dca8d2a52665bbd8fbe6cbcae1
Reina Ogo <reinasuzuki853@gmail.com>
2022-02-05 17:55:27 +0900

    initial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+        <Toast v-if="showToast" />
+    </transition>
     <Todos @badValue="triggerToast" />
+        <!-- <div v-if="display">Hi reina</div> -->
   </div>
 </template>
 
@@ -12,19 +15,72 @@ import Todos from '../components/Todos'
 
 export default {
   components: { Toast, Todos },
+//   data(){
+//       return{
+//           display: false
+
+//       }
+//   },
   setup() {
     const showToast = ref(false)
+    const display = ref(false)
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+    return { showToast, triggerToast, display }
+  },
+  methods:{
+      displayMsg(){
+          this.display = !this.display;
+      }
   }
+
+
 }
 </script>
 
 <style>
+/* enterclasses */
+.toast-enter-from{
+    opacity: 0;
+    transform: translateY(-60)
+}
+.toast-enter-to{
+    opacity:1;
+    transform: translateX(0)
+}
+.toast-enter-active{
+    transition: all 3s ease;
 
+}
+
+
+/* this is the state at the beggining */
+.fade-enter-from{
+ opacity:0;
+}
+
+.fade-enter-to{
+opacity:1;
+}
+/* how do we transit from one place to another */
+.fade-enter-from-active{
+transition: all 2s ease;
+}
+
+.fade-leave-from{
+    opacity:1;
+
+}
+/* where we want to end up */
+.fade-leave-to{
+    opacity: 0;
+}
+
+.fade-leave-active{
+transition: all 1s ease;
+}
 </style>
